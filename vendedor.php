40ed1f5c5c2f1a4c4a35c590680e979b52378209
LicLuisMancilla <mancillaperedoluis@pedropoveda.edu.bo>
2026-09-11 12:26:41 -0400

    todo right
    masomenos arreglado

--- vendedor.php
+++ vendedor.php
@@ -475,12 +475,12 @@ $resPedidos = $conn->query($sqlPedidos);
                     <div class="bg">
                         <div class="bc">
                             <h2>Pedido #<?php echo sprintf('%03d', $ped['id']); ?></h2>
-                            <p><?php echo htmlspecialchars($ped['Nombre']); ?></p>
+                            <p><?php echo htmlspecialchars($ped['nombre']); ?></p>
                         </div>
                         <div class="bd">
                             <h3>Bs <?php echo number_format($ped['total_calculado'] ? $ped['total_calculado'] : 0, 2); ?></h3>
-                            <p><?php echo date('d M Y', strtotime($ped['Fecha'])); ?></p>
-                            <p><?php echo date('h:i A', strtotime($ped['Fecha'])); ?></p>
+                            <p><?php echo date('d M Y', strtotime($ped['fecha'])); ?></p>
+                            <p><?php echo date('h:i A', strtotime($ped['fecha'])); ?></p>
                         </div>
                     </div>
 
